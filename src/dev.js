f85e42fa5e5a329d846260157407f7f6e5ea5ae5
Nerexbcd <me@nerexbcd.dev>
2024-11-09 20:34:32 +0000

    Finished Page - Added Dev Mode Detection - Cleaned up the Project

--- src/dev.js
+++ src/dev.js
@@ -1,15 +1,23 @@
-var styles = document.getElementsByTagName("head")[0].getElementsByTagName("link");
+console.log("Current Host: " + window.location.host);
 
-for (var i = 0; i < styles.length; i++) {
-    styles[i].href = styles[i].href.replace("https://failover.nerexbcd.dev/", "/");
-}
+if (!window.location.host.includes("nerexbcd.dev")){
+    var styles = document.getElementsByTagName("head")[0].getElementsByTagName("link");
 
-var scripts = document.getElementsByTagName("head")[0].getElementsByTagName("script");
-
-for (var i = 0; i < scripts.length; i++) {
-    var script = document.createElement("script");
-    script.src = scripts[i].src.replace("https://failover.nerexbcd.dev/", "/");
-    script.type = "text/javascript";
-    document.getElementsByTagName("head")[0].removeChild(scripts[i]);
-    document.getElementsByTagName("head")[0].appendChild(script);
+    for (var i = 0; i < styles.length; i++) {
+        styles[i].href = styles[i].href.replace("https://failover.nerexbcd.dev/", "/");
+    }
+    
+    var scripts = document.getElementsByTagName("head")[0].getElementsByTagName("script");
+    
+    for (var i = 0; i < scripts.length; i++) {
+        var script = document.createElement("script");
+        script.src = scripts[i].src.replace("https://failover.nerexbcd.dev/", "/");
+        script.type = "text/javascript";
+        document.getElementsByTagName("head")[0].removeChild(scripts[i]);
+        document.getElementsByTagName("head")[0].appendChild(script);
+    }
+    
+    console.log("Dev mode Enabled");
+} else {
+    console.log("Dev mode Disabled");
 }
