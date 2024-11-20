8a1015aea751b111dd529a1398284ecd3c79de20
Nerexbcd <me@nerexbcd.dev>
2024-11-20 09:20:27 +0000

    Added MultiMode to page

--- src/index.js
+++ src/index.js
@@ -4,3 +4,26 @@ function loadSpacers() {
         spacers[i].style.height = document.getElementById(spacers[i].id.split('-')[0]).offsetHeight + 'px'
     }
 }
+
+function setContents() {
+    var mode = window.location.search.split('mode=')[1].split('&')[0]
+
+    var icon, text
+    switch (mode) {
+        case '404':
+            icon = 'search-x'
+            text = 'The page you are looking for does not exist! ㅤ'
+            break
+        default:
+            icon = 'triangle-alert'
+            text = 'If You are seeing this page, it means the service is Offline or Under Maintenance! ㅤ'
+            break
+    }
+
+
+    window.getElementById('icon').setAttribute('data-lucide', icon)
+    var contents = window.getElementsByClassName('bannerText')
+    for (var i = 0; i < contents.length; i++) {
+        contents[i].innerText = text
+    }
+}
