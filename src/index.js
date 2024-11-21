7d466cb8ad330cb56e059ea1788c3df27f4768bc
Nerexbcd <me@nerexbcd.dev>
2024-11-21 22:30:36 +0000

    Changed mode selection system and updated some css

--- src/index.js
+++ src/index.js
@@ -5,26 +5,31 @@ function loadSpacers() {
     }
 }
 
-function autoMode() { 
-    var searchParams = new URLSearchParams(window.location.search)
-    setMode(searchParams.has('mode') ? searchParams.get('mode') : 'Default' ) }
-
-function setMode(mode) {
-    var icon, text, mainText
-    switch (mode) {
+function autoMode() {
+    var icon, text, mainText, bannerColor
+    switch (window.location.pathname.split('/')[1]) {
         case '404':
             icon = 'search-x'
+            bannerColor = 'bg-red-300'
             text = 'The page you are looking for does not exist! ㅤ'
             mainText = '404 - Not Found'
             break
+        case 'dev':
+            icon = 'pencil-ruler'
+            bannerColor = 'bg-yellow-300'
+            text = 'The page you are looking for is in Development! ㅤ'
+            mainText = 'Service In Development'
+            break
         default:
             icon = 'triangle-alert'
+            bannerColor = 'bg-red-300'
             text = 'If You are seeing this page, it means the service is Offline or Under Maintenance! ㅤ'
             mainText = 'Service Unavailable'
             break
     }
 
     document.getElementById('icon').setAttribute('data-lucide', icon)
+    document.getElementById('devBanner').classList.add(bannerColor)
     document.getElementById('mainText').innerText = mainText
     document.getElementsByTagName('title')[0].innerText = mainText + ' | Nerexbcd'
     var contents = document.getElementsByClassName('bannerText')
@@ -34,6 +39,7 @@ function setMode(mode) {
 }
 
 function finalizePage() {
+    autoMode()
     lucide.createIcons();
     loadSpacers();
 }
